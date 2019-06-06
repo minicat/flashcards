fe859e6c5d4f2cb5a0a83c931ed57e9b03485e2a
Emma Yeap <emmacyeap@gmail.com>
2019-06-06 16:31:44 -0700

    basic airtable data fetch

--- src/FlashcardApp.tsx
+++ src/FlashcardApp.tsx
@@ -1,5 +1,9 @@
+import Airtable from 'airtable';
 import React from 'react';
 import './flashcards.css';
+
+const BASE_NAME = 'Vocab list';
+const VIEW_NAME = 'Main list';
 
 interface RecordFields {
   // id: string, need this?
@@ -10,19 +14,59 @@ interface RecordFields {
   added: Date,
   correct: number,
   attempts: number,
-  lastAttempt?: Date,
+  lastTested?: Date,
+}
+
+type RecordMap = {[id: string]: RecordFields};
+
+interface FlashcardAppProps {
+  base: Airtable.Base;
 }
 
 interface FlashcardAppState {
-  records: {[id: string]: RecordFields},
+  records?: RecordMap,
   currentTestSet?: string[],
 }
 
-export class FlashcardApp extends React.Component<{}, FlashcardAppState> {
+export class FlashcardApp extends React.Component<FlashcardAppProps, FlashcardAppState> {
+  constructor(props: FlashcardAppProps) {
+    super(props);
+    this.fetchRecords();
+
+    this.state = {};
+  }
+
+  fetchRecords = async () => {
+    // TODO: can move this into a helper
+    let rawRecords = await this.props.base(BASE_NAME).select({
+      view: VIEW_NAME
+    }).all();
+
+    let records: RecordMap = {};
+
+    rawRecords.forEach(rawRecord => {
+      let fields: any = rawRecord.fields;
+      // XXX: what's a nicer way to do this?
+      let record: RecordFields = {
+        english: fields["English"],
+        pinyin: fields["Pinyin"],
+        chinese: fields["Chinese"],
+        category: fields["Category"],
+        added: new Date(fields["Added"]),
+        correct: fields["Correct"],
+        attempts: fields["Attempts"],
+        lastTested: fields["Last Tested"] ? new Date(fields["Last Tested"]) : undefined,
+      };
+      records[rawRecord.id as string] = record;
+    })
+
+    this.setState({records: records});
+  }
+
   render() {
     return (
       <div className="main">
-        stuff
+       hello
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import Airtable from 'airtable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {FlashcardApp} from './FlashcardApp';
+let base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE_API_KEY}).base('appz6ksxJMtsLEs7R');
 
-ReactDOM.render(<FlashcardApp />, document.getElementById('root'));
+ReactDOM.render(<FlashcardApp base={base} />, document.getElementById('root'));
